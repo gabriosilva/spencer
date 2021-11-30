6fb5630a813945eb6669f4ec0a5293255a3577dc
gabriosilva <gabrio.lina@hotmail.com>
2021-11-30 12:55:07 -0300

    chore(services): create api constants file

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://localhost:5000/api";
+
+export const LOGIN_URL = `${BASE_URL}/auth/login`;
+export const REGISTER_URL = `${BASE_URL}/auth/register`;
+export const AUTH_URL = `${BASE_URL}/auth/isAuth`;
